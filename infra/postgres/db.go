3fb77df173ac014c2cc75e728fb9aaeb714e2c23
Khairozzaman <khairozzaman91@gmail.com>
2026-07-22 03:06:21 +0600

    feat: setup PostgreSQL database connection with sqlx

---
+++ infra/postgres/db.go
@@ -0,0 +1,24 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func load() string {
+	return "host=localhost port=5432 user=postgres password=2173 dbname=jobportal sslmode=disable"
+}
+
+func GetConnect() (*sqlx.DB, error) {
+	dbSource := load()
+
+	db, err := sqlx.Connect("postgres", dbSource)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return db, nil
+}
